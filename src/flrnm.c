2d9ca72f6249b858d0cfcbd79e0e0368a5157f69
TahDam <alexandr.v.zaharchuk@gmail.com>
2016-02-07 17:04:59 +0300

    Added string.h.txt

--- src/flrnm.c
+++ src/flrnm.c
@@ -3,13 +3,24 @@
 //
 
 #include <string.h>
+#include <printf.h>
+#include <stdio.h>
 
 const char *flrnm(const char *fln) {
     char flnMSS [strlen(fln)];
     char * lstchar;
 
+    /*
     for (int i = 0; i < strlen(flnMSS); ++i) {
-        lstchar = flnMSS[i];
+        // lstchar = flnMSS[i];
+
+
     }
+     */
+
+    int tmp = sizeof(flnMSS);
+    printf("Size of flnMSS = %i", tmp);
+
+    return 0;
 
 }
